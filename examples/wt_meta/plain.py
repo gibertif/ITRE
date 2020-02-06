Corrected the calculation of the bias matrix so that it start from 0 and get to N-1 evaluation

examples/wt_meta/plain.py
@@ -12,7 +12,7 @@ if os.path.isfile("pyitre.json"):
         directives = json.load(json_file)
 
 
-i_bias = np.loadtxt('bias',usecols=1)
+i_bias = np.loadtxt('bias',usecols=1,skiprows=2)
 
 it = itre.Itre()
 it.from_dict(directives)
@@ -24,6 +24,10 @@ it.calculate_c_t()
 end=time.time()
 raw_time=end-start
 print("execution completed in {}".format(raw_time))
+
+plt.plot(it.instantaneous_bias)
+plt.plot(i_bias[::it.stride])
+plt.show()
 
 ref_ct = it.ct[-1].T
 np.savetxt('c_t.dat',ref_ct)

itre/atlas.py
@@ -30,19 +30,19 @@ class Atlas(object):
 
         for i in range(n_evals):
             ref_index= int(i*stride)
-            for j in range(i+1,n_evals):
+            for j in range(i,n_evals-1):
                 lower_index = int(j*stride)
                 upper_index = int((j+1)*stride)
                 bias_sum = 0.0
                 for t in range(lower_index,upper_index):
                     for minimum in range(n_minima):
                         start = int(minimum*dims) ; end = int(minimum*dims)+dims
-                        switch = thetas[ref_index,minimum]*thetas[t-1,minimum]
-                        sum_bias += self.kernel(colvars[ref_index,start:end],colvars[t-1,start:end],sigmas[t-1,start:end])*heights[t-1]*switch
+                        switch = thetas[ref_index,minimum]*thetas[t,minimum]
+                        sum_bias += self.kernel(colvars[ref_index,start:end],colvars[t,start:end],sigmas[t,start:end])*heights[t]*switch
 
-                    sum_bias += wall[t-1]
+                    sum_bias += wall[t]
 
-                bias_matrix[j,i] = bias_matrix[j-1,i] + bias_sum
+                bias_matrix[j+1,i] = bias_matrix[j,i] + bias_sum
 
         return bias_matrix
 
@@ -67,17 +67,17 @@ class Atlas(object):
                 bias_matrix[i,i] += sum_bias + wall[k]
 
         for i in range(n_evals):
-            for j in range(i+1,n_evals):
+            for j in range(i,n_evals-1):
                 bias_sum = 0.0
                 for t in range(j*stride,(j+1)*stride):
                     for minimum in range(n_minima):
                         start = int(minimum*dims) ; end = int(minimum*dims)+dims
-                        dist = (colvars[i*stride,start:end]-colvars[t-1,start:end])/sigmas[t-1,start:end]
+                        dist = (colvars[i*stride,start:end]-colvars[t,start:end])/sigmas[t,start:end]
                         dist2 = 0.5 * dist.dot(dist)
-                        sum_bias += np.exp(-dist2)*heights[t-1]*thetas[i*stride,minimum]*thetas[t-1,minimum]
+                        sum_bias += np.exp(-dist2)*heights[t]*thetas[i*stride,minimum]*thetas[t,minimum]
 
-                    sum_bias += wall[t-1]
+                    sum_bias += wall[t]
 
-                bias_matrix[j,i] = bias_matrix[j-1,i] + bias_sum
+                bias_matrix[j+1,i] = bias_matrix[j,i] + bias_sum
 
         return bias_matrix

itre/metadynamics.py
@@ -13,7 +13,7 @@ class Metadynamics(object):
         dist = (a-b)/c
         dist = 0.5 * dist.dot(dist)
         return np.exp(-dist)
-        
+
     @staticmethod
     @nb.jit
     def calculate_bias_matrix_nb(colvars,sigmas,heights,wall,n_evals,stride,dims):
@@ -28,14 +28,14 @@ class Metadynamics(object):
                 bias_matrix[i,i] += np.exp(-dist2)*heights[k] + wall[k]
 
         for i in range(n_evals):
-            for j in range(i+1,n_evals):
+            for j in range(i,n_evals-1):
                 bias_sum = 0.0
                 for t in range(j*stride,(j+1)*stride):
-                    dist = (colvars[i*stride]-colvars[t-1])/sigmas[t-1]
+                    dist = (colvars[i*stride]-colvars[t])/sigmas[t]
                     dist2 = 0.5 * dist.dot(dist)
-                    bias_sum += np.exp(-dist2)*heights[t-1] + wall[t-1]
+                    bias_sum += np.exp(-dist2)*heights[t] + wall[t]
 
-                bias_matrix[j,i] = bias_matrix[j-1,i] + bias_sum
+                bias_matrix[j+1,i] = bias_matrix[j,i] + bias_sum
 
         return bias_matrix
 
@@ -49,13 +49,13 @@ class Metadynamics(object):
 
         for i in range(n_evals):
             ref_index= int(i*stride)
-            for j in range(i+1,n_evals):
+            for j in range(i,n_evals-1):
                 lower_index = int(j*stride)
                 upper_index = int((j+1)*stride)
                 bias_sum = 0.0
                 for t in range(lower_index,upper_index):
-                    bias_sum += self.kernel(colvars[ref_index],colvars[t-1],sigmas[t-1])*heights[t-1]  + wall[t-1]
+                    bias_sum += self.kernel(colvars[ref_index],colvars[t],sigmas[t])*heights[t]  + wall[t]
 
-                bias_matrix[j,i] = bias_matrix[j-1,i] + bias_sum
+                bias_matrix[j+1,i] = bias_matrix[j,i] + bias_sum
 
         return bias_matrix